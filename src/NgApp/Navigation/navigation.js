1516d956e84f84da116739203d7d61147d870fbc
Jesse Hemstad <jesse.hemstad@lotpath.com>
2014-02-17 11:54:23 -0800

    navigation now its own module, routes moved

---
+++ src/NgApp/Navigation/navigation.js
@@ -0,0 +1,22 @@
+﻿angular.module('app.navigation', ['ngRoute']);
+
+angular.module('app.navigation')
+    .controller('NavigationCtrl', ['$scope', '$location', NavigationCtrl]);
+
+function NavigationCtrl($scope, $location) {
+    $scope.title = 'Strappy';
+    $scope.navItems = [
+        { title: " Home",   route: "/",  icon: "fa fa-home" },
+        { title: "About", route: "/about", icon: "" }
+    ];
+
+    $scope.isCollapsed = true;
+
+    $scope.setRoute = function (route) {
+        $location.path(route);
+    };
+
+    $scope.isActive = function(route) {
+        return ($location.path() == route);
+    };
+}
--- src/NgApp/app.js
+++ src/NgApp/app.js
@@ -1,28 +1,9 @@
-﻿(function () {
-    'use strict';
+﻿var app = angular.module('app', [
+    'app.navigation',
+    'ngSanitize',
+    'ui.bootstrap'
+]);
 
-    var app = angular.module('app', [
-        'ngRoute',
-        'ngSanitize',
-        'ui.bootstrap'
-    ]);
-
-    app.config(['$routeProvider', function($routeProvider) {
-        $routeProvider
-            .when('/', {
-                templateUrl: 'NgApp/Home/home.html',
-                controller: 'HomeCtrl'
-            })
-            .when('/about', {
-                templateUrl: 'NgApp/About/about.html',
-                controller: 'AboutCtrl'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
+app.run(['$q', '$rootScope',
+    function ($q, $rootScope) {
     }]);
-
-    app.run(['$q', '$rootScope',
-        function ($q, $rootScope) {
-        }]);
-})();
